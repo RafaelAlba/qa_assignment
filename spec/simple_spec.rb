11e15bb11e34d329f5d261a9a442a8656cac3c78
Kevin Dishman <kdishman@thoughtworks.com>
2013-10-23 10:03:37 -0700

    Kevin - changed double quotes to single to make intelliJ quit whining

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -1,16 +1,16 @@
-require "selenium-webdriver"
+require 'selenium-webdriver'
 
-describe "Test" do
-  it "should search Wikipedia for information about Pandas" do
+describe 'Test' do
+  it 'should search Wikipedia for information about Pandas' do
     driver = Selenium::WebDriver.for :firefox
-    driver.navigate.to "http://www.wikipedia.org"
+    driver.navigate.to 'http://www.wikipedia.org'
 
     element = driver.find_element(:id, 'searchInput')
-    element.send_keys "Panda"
+    element.send_keys 'Panda'
     button = driver.find_element(:name, 'go')
     button.click
     heading_text = driver.find_element(:id, 'firstHeading').text
-    heading_text.should == "Giant panda"
+    heading_text.should == 'Giant panda'
     driver.quit
   end
 end
