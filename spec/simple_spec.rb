518c2e0b9d73ac4a2ceee04ef954296f981399b1
Kevin Dishman <kdishman@thoughtworks.com>
2013-10-23 11:58:50 -0700

    Kevin - added another test which requires waiting to complete sucessfully

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -13,4 +13,20 @@ describe 'Test' do
     heading_text.should == 'Giant panda'
     driver.quit
   end
+
+  it 'should see Wikipedia link when you search bing for information about Pandas' do
+    driver = Selenium::WebDriver.for :firefox
+    driver.navigate.to 'http://www.bing.com'
+
+    search_box = driver.find_element(:id, 'sb_form_q')
+    search_box.send_keys 'Panda'
+    search_box.send_keys :enter
+
+    search_results = driver.find_element(:id, 'b_results')
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    begin
+      wait.until { search_results.text.should include("Giant panda - Wikipedia, the free encyclopedia") }
+    end
+    driver.quit
+  end
 end
